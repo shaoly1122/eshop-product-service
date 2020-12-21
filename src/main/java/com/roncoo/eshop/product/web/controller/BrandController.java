d484a8a2d7fec4f06fa6a3280fd538233bb7a768
sly <shaoly1122@vip.sina.com>
2020-12-22 02:28:39 +0800

    second commit

--- src/main/java/com/roncoo/eshop/product/web/controller/BrandController.java
+++ src/main/java/com/roncoo/eshop/product/web/controller/BrandController.java
@@ -1,5 +1,8 @@
 package com.roncoo.eshop.product.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -55,5 +58,15 @@ public class BrandController {
 			e.printStackTrace(); 
 		}
 		return new Brand();
+	}
+	@RequestMapping("/findByIds") 
+	@ResponseBody
+	public List<Brand> findByIds(String ids){
+		try {
+			return brandService.findByIds(ids);
+		} catch (Exception e) {
+			e.printStackTrace(); 
+		}
+		return new ArrayList<Brand>();
 	}
 }
